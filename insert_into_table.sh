f7a697405205e093bcc5766e7c103066db4f7fc5
BasmalaSalem <basmala.mahmoud.salem@gmail.com>
2024-07-23 15:40:32 +0300

    add available tables in the selected database as option for user to select from in insert_int_table

--- insert_into_table.sh
+++ insert_into_table.sh
@@ -2,15 +2,50 @@
 source ./validation.sh
 
 insert_into_table() {
-    # local DBName="$1"
-    # local TableName="$2"
+    local DBName="$1"
 
-    local DBName="db1"
-    local TableName="t1"
+    # Check if the database directory exists
+    if [[ ! -d "$DBName" ]]; then
+        echo "Error: Database '$DBName' not found."
+        return
+    fi
 
-    # Check if the database and table exist
+    # List tables in the database
+    local tables=($(ls -p "$DBName" | grep -v /))
+    local num_tables=${#tables[@]}
+
+    if [ $num_tables -eq 0 ]; then
+        echo "No tables found in the database."
+        return
+    fi
+
+    echo "Available tables in database '$DBName':"
+    for i in "${!tables[@]}"; do
+        echo "$((i + 1)). ${tables[$i]}"
+    done
+
+    # Prompt user to select a table to insert into
+    local choice
+    while true; do
+        read -p "Enter the number of the table to insert into (or 0 to cancel): " choice
+
+        if [[ $choice =~ ^[0-9]+$ ]] && ((choice >= 0 && choice <= num_tables)); then
+            break
+        else
+            echo "Invalid input. Please enter a valid number."
+        fi
+    done
+
+    if [ $choice -eq 0 ]; then
+        echo "Insert operation canceled."
+        return
+    fi
+
+    local TableName="${tables[$((choice - 1))]}"
+
+    # Check if the selected table exists
     if [[ ! -f "$DBName/$TableName" ]]; then
-        echo "Error: Database or table not found."
+        echo "Error: Table '$TableName' not found."
         return
     fi
 
@@ -83,6 +118,9 @@ insert_into_table() {
     # Add a newline at the end of the record
     echo "" >>"$DBName/$TableName"
     echo "Record inserted successfully."
+
+    sleep 2
+    mainmenu
 }
 
 # insert_into_table
